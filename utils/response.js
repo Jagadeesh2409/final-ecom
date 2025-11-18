026cda768a94c6790eb447bfaae67d33dfe8f4f2
Jagadeesh2409 <jagadeesh242004@gmail.com>
2025-11-18 11:46:02 +0530

    upload functionality implemented

--- utils/response.js
+++ utils/response.js
@@ -1,13 +1,16 @@
 const successResponse = (res,data,message = "success",status =200) =>{
-    return res.status(status).json({message,data})
+    return res.status(status).json({success:"ok",message,data})
 }
 
 const errorResponse = (res,message = 'failed',status = 400) => {
-    return res.status(status).json({message})
+    return res.status(status).json({success:"fail",message})
 }
 
 const response = {
-    ISE:"internal server error"
+    ISE:"internal server error",
+
+    UPLOAD_SUCCESS:"upload success",
+    UPLOAD_FAILED:"upload failed",
 }
 
 module.exports = {response,successResponse,errorResponse}
